5e885bb9b5faed25dc56cc094ee478f1117c536b
Jameson Gelarman <96761338+Ashtreus1@users.noreply.github.com>
2024-05-17 19:30:52 +0000

    fixing conflicts for building the app

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,15 +1,12 @@
 import Team from "@/components/AboutUs/Team";
 import React from "react";
 
-const AboutUs = ({ className }: { className: string }) => {
-
+const AboutUs = () => {
     return (
-        <div
-            className={`${className} flex flex-row max-w-4xl mx-auto py-24 gap-4`}
-        >
+        <div className="flex flex-row max-w-4xl mx-auto py-24 gap-4">
             <div className="gap-4 flex flex-col mt-4">
-                <h1 className={`text-6xl font-medium`}>Officers</h1>
-                <h2 className={`text-3xl text-gray-800 font-light`}>
+                <h1 className="text-6xl font-medium">Officers</h1>
+                <h2 className="text-3xl text-gray-800 font-light">
                     The great people <br /> behind the organization
                 </h2>
             </div>
