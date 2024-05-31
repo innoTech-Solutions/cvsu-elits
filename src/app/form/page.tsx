d9bf9fafdf7c5635a24affcd5453380288eeaef6
Jameson Gelarman <96761338+Ashtreus1@users.noreply.github.com>
2024-05-31 01:08:15 +0000

    test commit

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,8 +1,20 @@
-import AuthContainer from '@/components/Form/AuthContainer'
+"use client"
+import { motion, AnimatePresence } from 'framer-motion';
+import AuthContainer from '@/components/Form/AuthContainer';
+
 const Form = () => {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <AuthContainer/>
+            <AnimatePresence>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.5 }}
+                >
+                    <AuthContainer />
+                </motion.div>
+            </AnimatePresence>
         </div>
     );
 };
